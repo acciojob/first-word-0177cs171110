dd9a19bbe2e34096bdaefd709f6c9d615e614292
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-12 12:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	if (!s.includes(' ')) {
+    // If the string doesn't contain any space, return the entire string
+    return s;
+  } else {
+    // Otherwise, find the index of the first space and extract the substring before it
+    return s.substring(0, s.indexOf(' '));
+  }
 }
 
 // Do not change the code below
